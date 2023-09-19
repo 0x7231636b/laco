8b3b2bcb39e677fd1239d8ee3c4f7ee35e9372e4
Markus Hallmann <markus@hallmann.pw>
2023-09-19 22:43:01 +0200

    Cleaned the tests and defined constants.

--- test/Dice.test.ts
+++ test/Dice.test.ts
@@ -1,17 +1,18 @@
 import { newNumber } from "../components/dice";
 
 const LIMIT = 1000000;
+const DELTA = 2000;
 const DICE_FACES = 6;
+const averageFaceCount = LIMIT / DICE_FACES;
 
 describe("Random number test", () => {
-  it("is equally distributed after rolling dice a lot", () => {
+  it("is equally distributed after rolling the dice a lot", () => {
     const numbersMap = createMap();
 
     var counter = LIMIT;
     while (counter--) {
       const currentValue = newNumber();
       const numberCount = numbersMap.get(currentValue);
-
       if (numberCount === undefined || numberCount === null)
         throw new Error(
           "Check if definition of newNumber() has changed or check createMap() setup in test file Dice.test.ts"
@@ -20,28 +21,28 @@ describe("Random number test", () => {
       numbersMap.set(currentValue, numberCount + 1);
     }
 
-    const delta = 10000;
-    const averageFaceCount = LIMIT / DICE_FACES;
-
-    const deltaInRange = (valueToCheck: number) => {
-      return (
-        valueToCheck > averageFaceCount - delta &&
-        valueToCheck < averageFaceCount + delta
-      );
-    };
-
     numbersMap.forEach((value, key) => {
-      expect(deltaInRange(value));
+      expect(deltaInRange(value)).toBeTruthy();
     });
   });
-});
 
-function createMap() {
-  const numbersMap = new Map<number, number>();
-
-  for (var i = 1; i <= DICE_FACES; i++) {
-    numbersMap.set(i, 0);
+  function deltaInRange(valueToCheck: number) {
+    const deltaInRange = Math.abs(valueToCheck - averageFaceCount) < DELTA;
+    if (!deltaInRange) {
+      console.error(
+        `Delta is out of range with value ${valueToCheck - averageFaceCount}`
+      );
+    }
+    return deltaInRange;
   }
 
-  return numbersMap;
-}
+  function createMap() {
+    const numbersMap = new Map<number, number>();
+
+    for (var i = 1; i <= DICE_FACES; i++) {
+      numbersMap.set(i, 0);
+    }
+
+    return numbersMap;
+  }
+});
